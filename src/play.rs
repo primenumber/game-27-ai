e1c732d981478be94498d68a29b43b4d3d64ae2d
primenumber <prime@kmc.gr.jp>
2021-03-03 10:36:43 +0900

    Fix time limit

--- src/play.rs
+++ src/play.rs
@@ -283,7 +283,7 @@ impl<T: TGame27, E: TEvaluator<Game = T>> AlphaBetaPlayer<T, E> {
         let max_score = self.evaluator.max_score();
         let start = Instant::now();
         let mut action = None;
-        let time_limit = 1000; // as milliseconds
+        let time_limit = 50; // as milliseconds
         let vnexts = self.move_ordering_by_eval(b);
         for depth in 8..40 { // iterative deepening
             self.memo.clear();
